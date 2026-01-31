ddef1edeff7d24d47adffe316d3435cdc5204da7
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-01-31 16:41:07 +0530

    Changes made to deploy correctly

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://label-lens-wenv.onrender.com",
     credentials: true,
 }));
 
